0945237fd47c9a41fee963f1632ea75f1baf912d
otabekmirzakhmedov <otabek.mirzakhmedov@gmail.com>
2024-07-08 09:17:29 +0500

    feat: combining app

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Component } from 'react';
+import { getPeople } from './services/SwapiService';
+import { Person } from './types/Person';
+import PersonCard from './components/card-component/PersonCard';
+import SearchComponent from './components/search-component/SearchComponent';
+import ErrorBoundary from './components/error-bound-component/ErrorBound';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface PeopleListState {
+  people: Person[];
 }
 
-export default App
+class App extends Component<Record<string, never>, PeopleListState> {
+  constructor(props: Record<string, never>) {
+    super(props);
+    this.state = {
+      people: [],
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const data = await getPeople(1);
+      this.setState({ people: data });
+    } catch (err) {
+      console.error('Failed to fetch people:', err);
+    }
+  }
+
+  handleSearchResults = (results: Person[]) => {
+    this.setState({ people: results });
+  };
+
+  render() {
+    const { people } = this.state;
+
+    return (
+      <div>
+        <h1>Star Wars Characters</h1>
+        <ErrorBoundary>
+          <SearchComponent onSearchResults={this.handleSearchResults} />
+          <div className='people-list'>
+            {people.map((person) => (
+              <PersonCard key={person.name} person={person} />
+            ))}
+          </div>
+        </ErrorBoundary>
+      </div>
+    );
+  }
+}
+
+export default App;
